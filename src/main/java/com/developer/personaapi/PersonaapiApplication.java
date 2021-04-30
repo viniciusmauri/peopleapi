7c4f7aa305abf4e1335d22c8066c250991122004
Mauri <viniciusmauri@outlook.com.br>
2021-04-30 11:45:15 -0300

    START PROJECT

---
+++ src/main/java/com/developer/personaapi/PersonaapiApplication.java
@@ -0,0 +1,13 @@
+package com.developer.personaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaapiApplication.class, args);
+	}
+
+}
