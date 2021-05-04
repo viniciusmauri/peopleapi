31a4694fd0f38d2c851b55cdd5a8bea119797729
Mauri <viniciusmauri@outlook.com.br>
2021-05-04 09:12:51 -0300

    Ajustando Readme e refarorando alguns métodos

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # People Api
+Api para criação de pessoas. Intuito da criação deste é para praticar as técnicas e tecnologias aqui aplicadas.
+Esta Api foi criada utilizando o framework SpringBoot na versão *2.4.5*.
+# Pré-requisitos
+* Java 11 ou superior
+* Maven 3.6.3 ou superior
+# Como executar o projeto
+```shell script
+mvn spring-boot:run
+```
 
-Projeto criado referente ao que foi pedido no bootcamp da avanade(digital innovation one)
+Após executar o comando acima, basta apenas abrir o seguinte endereço e visualizar a execução do projeto:
+```shell script
+http://localhost:8080/api/v1/people
+```
--- src/main/java/com/developer/personaapi/controller/PersonController.java
+++ src/main/java/com/developer/personaapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.developer.personaapi.dto.request.PersonDTO;
 import com.developer.personaapi.dto.response.MessageResponseDTO;
 import com.developer.personaapi.exception.PersonNotFoundException;
 import com.developer.personaapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,15 +22,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
